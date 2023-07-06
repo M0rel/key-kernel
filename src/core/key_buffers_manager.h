d005aa7dadf2997f4618593b262e0cb0eb997cf8
Vadym Kharchuk <vadymkharchuk.wk@gmail.com>
2023-07-06 14:24:57 +0300

    buffer_manager: Add consumer report
    Signed-off-by: Vadym Kharchuk <vadymkharchuk.wk@gmail.com>

--- src/core/key_buffers_manager.h
+++ src/core/key_buffers_manager.h
@@ -9,4 +9,9 @@ typedef struct usb_report {
         uint8_t keys[MAX_PRESSED_IN_REPORT];
 } usb_report_st_t;
 
+typedef struct consumer_report {
+        uint8_t id;
+        uint8_t keys;
+} consumer_report_st_t;
+
 void prepare_send_buffer(keyboard_desc_st_t *key_desc, usb_report_st_t *report);
